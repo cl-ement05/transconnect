100ef2868655555963c86860364c9fe8d77d0986
alixg12 <alix.gleizes@gmail.com>
2025-04-16 19:16:52 +0200

    commande_1

--- Commande.cs
+++ Commande.cs
@@ -6,61 +6,33 @@ namespace transconnect
         private Client client;
         private string villeDepart;
         private string villeArrivee;
+        private Vehicule vehicule;
         private Chauffeur chauffeur;
         private DateTime dateCommande;
 
         public Commande(int numeroCommande, Client client, string villeDepart, string villeArrivee,
-                         Chauffeur chauffeur, DateTime dateCommande)
+                        Vehicule vehicule, Chauffeur chauffeur, DateTime dateCommande)
         {
             this.numeroCommande = numeroCommande;
             this.client = client;
             this.villeDepart = villeDepart;
             this.villeArrivee = villeArrivee;
+            this.vehicule = vehicule;
             this.chauffeur = chauffeur;
             this.dateCommande = dateCommande;
         }
 
-        public int NumeroCommande
-        {
-            get { return numeroCommande; }
-            set { numeroCommande = value; }
-        }
-
-        public Client Client
-        {
-            get { return client; }
-            set { client = value; }
-        }
-
-        public string VilleDepart
-        {
-            get { return villeDepart; }
-            set { villeDepart = value; }
-        }
-
-        public string VilleArrivee
-        {
-            get { return villeArrivee; }
-            set { villeArrivee = value; }
-        }
-
-
-
-        public Chauffeur Chauffeur
-        {
-            get { return chauffeur; }
-            set { chauffeur = value; }
-        }
-
-        public DateTime DateCommande
-        {
-            get { return dateCommande; }
-            set { dateCommande = value; }
-        }
+        public int NumeroCommande { get { return numeroCommande; } set { numeroCommande = value; } }
+        public Client Client { get { return client; } set { client = value; } }
+        public string VilleDepart { get { return villeDepart; } set { villeDepart = value; } }
+        public string VilleArrivee { get { return villeArrivee; } set { villeArrivee = value; } }
+        public Vehicule Vehicule { get { return vehicule; } set { vehicule = value; } }
+        public Chauffeur Chauffeur { get { return chauffeur; } set { chauffeur = value; } }
+        public DateTime DateCommande { get { return dateCommande; } set { dateCommande = value; } }
 
         public override string ToString()
         {
-            return $"[Commande #{NumeroCommande}] {VilleDepart} → {VilleArrivee}  | {DateCommande.ToShortDateString()}";
+            return "Commande : " + numeroCommande + " | " + villeDepart + " -> " + villeArrivee + " | Date : " + dateCommande.ToShortDateString() + " | Véhicule : " + vehicule;
         }
     }
 }
