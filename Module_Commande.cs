98fd7fa5bf12248bbecf09b5747773e6db6a069a
alixg12 <alix.gleizes@gmail.com>
2025-04-16 13:05:19 +0200

    Modif_com_1

--- Module_Commande.cs
+++ Module_Commande.cs
@@ -67,5 +67,96 @@ namespace transconnect
             */
         
         }
+
+        public void Modifier_commande(int numeroCommande)
+        {
+            Commande? commande=commandes.Find(c => c.NumeroCommande==numeroCommande);
+            if(commande == null)
+            {
+                Console.WriteLine("Commande introuvable");
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("\nCommande actuelle :");
+                Console.WriteLine(commande.ToString());
+                Console.WriteLine("\nQue souhaitez-vous modifier ?");
+                Console.WriteLine("1. Modifier le trajet (ville de départ et d'arrivée)");
+                Console.WriteLine("2. Modifier la date de commande");
+                Console.WriteLine("3. Terminer la modification");
+                Console.Write("Entrez votre choix (1-3) : ");
+
+                string choix = Console.ReadLine()!;
+
+                if(choix=="1")
+                {
+                    bool trajetModifie = false;
+                    Console.Write("Nouvelle ville de départ (laisser vide pour conserver '" + commande.VilleDepart + "') : ");
+                    string nvVilleDepart = Console.ReadLine()!;
+                    if (nvVilleDepart != "")
+                    {
+                        commande.VilleDepart = nvVilleDepart;
+                        trajetModifie = true;
+                    }
+
+                    Console.Write("Nouvelle ville d'arrivée (laisser vide pour conserver '" + commande.VilleArrivee + "') : ");
+                    string nvVilleArrivee = Console.ReadLine()!;
+                    if (nvVilleArrivee != "")
+                    {
+                        commande.VilleArrivee = nvVilleArrivee;
+                        trajetModifie = true;
+                    }
+
+                    /*if(trajetModifie)
+                    {
+                        // Redéterminer le nouveau parcours
+                        // Recalculer le prix
+                    }*/
+                }
+                else if(choix=="2")
+                {   
+                    Console.Write("Saisir nouvelle date de commande (JJ/MM/AAAA)");
+                    string nvDateStr = Console.ReadLine()!;
+                    if (nvDateStr != "")
+                    {
+                        DateTime nvdate=Convert.ToDateTime(nvDateStr);
+                        if(nvdate != commande.DateCommande)
+                        {
+                            if(!commande.Chauffeur.EstDisponible(nvdate))
+                            {
+                                Console.WriteLine("Le chauffeur actuel n'est pas disponible à cette date");
+                                Chauffeur? nvchauffeur=chauffeurs.Find(ch => ch.EstDisponible(nvdate));
+                                if(nvchauffeur==null)
+                                {
+                                    Console.WriteLine("Aucun chauffeur n'est disponible, la date ne sera pas modifié");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nouveau chaffeur assigné : " + nvchauffeur.Nom + " " + nvchauffeur.Prenom);
+                                    commande.Chauffeur.LivraisonsEffectuees.Remove(commande);
+                                    commande.Chauffeur=nvchauffeur;
+                                    nvchauffeur.LivraisonsEffectuees.Add(commande);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            commande.DateCommande=nvdate;
+                        }
+                    }
+                }
+
+                else if (choix == "3")
+                {
+                    Console.WriteLine("Modification terminée.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Choix invalide. Veuillez réessayer.");
+                }
+            }
+        }
     }
 }
