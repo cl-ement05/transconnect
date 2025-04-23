9e253b4340a7f9b1789f3fed29dc8976dc3b784c
alixg12 <alix.gleizes@gmail.com>
2025-04-23 14:17:33 +0200

    unit_test_2

--- unitTest/UnitTest1.cs
+++ unitTest/UnitTest1.cs
@@ -1,25 +1,29 @@
-
+using NUnit.Framework;
 using transconnect;
 
-
-namespace unitTest;
-
-public class UnitTest1
+namespace unitTest
 {
-    private DataState datastate;
-    private Module_statistiques module;
+    public class UnitTest1
+    {
+        private DataState datastate;
+        private Module_statistiques module;
 
+        [SetUp]
         public void Setup()
         {
             datastate = new DataState(new Dictionary<string, List<(string data, int weight)>>());
             module = new Module_statistiques(datastate);
         }
+
+        [Test]
         public void TestAfficherLivraisonsParChauffeur()
         {
-            Chauffeur chauffeur = new Chauffeur("1", "Michel", "Leblanc", DateTime.Now.AddYears(-30),"Bd Magenta", "leblanc@outlook.com", "0154278473", DateTime.Now.AddYears(-5), 2000);
+            Chauffeur chauffeur = new Chauffeur("1", "Michel", "Leblanc", DateTime.Now.AddYears(-30),
+                "Bd Magenta", "leblanc@outlook.com", "0154278473", DateTime.Now.AddYears(-5), 2000);
             datastate.salaries.Add(chauffeur);
 
-            Client client = new Client("1", "Marseulli", "Véronique", DateTime.Now.AddYears(-40),"Rue de la Gare", "vero.m@gmail.comm", "0698528488");
+            Client client = new Client("1", "Marseulli", "Véronique", DateTime.Now.AddYears(-40),
+                "Rue de la Gare", "vero.m@gmail.com", "0698528488");
             Voiture voiture = new Voiture("AB-123-CD", "Rouge", "Peugeot", 4);
             Commande commande = new Commande(1, client, "Paris", "Lyon", voiture, chauffeur, DateTime.Today);
             chauffeur.LivraisonsEffectuees.Add(commande);
@@ -28,4 +32,5 @@ public class UnitTest1
 
             Assert.AreEqual(1, chauffeur.LivraisonsEffectuees.Count);
         }
+    }
 }
