4299d54c1d942fb32924fbb64359cb02b35c5851
alixg12 <alix.gleizes@gmail.com>
2025-04-23 14:15:17 +0200

    unit_test_1

--- unitTest/UnitTest1.cs
+++ unitTest/UnitTest1.cs
@@ -1,13 +1,31 @@
+
+using transconnect;
+
+
 namespace unitTest;
 
 public class UnitTest1
 {
-    [Fact]
-    public void testUserFoundWords() {
-        // Joueur joueur = new Joueur("test");
-        // joueur.Add_Mot("mais");
-        // joueur.Add_Mot("ordinateur");
-        // Assert.True(joueur.Contient("mais"));
-        // Assert.False(joueur.Contient("plateau"));
-    }
+    private DataState datastate;
+    private Module_statistiques module;
+
+        public void Setup()
+        {
+            datastate = new DataState(new Dictionary<string, List<(string data, int weight)>>());
+            module = new Module_statistiques(datastate);
+        }
+        public void TestAfficherLivraisonsParChauffeur()
+        {
+            Chauffeur chauffeur = new Chauffeur("1", "Michel", "Leblanc", DateTime.Now.AddYears(-30),"Bd Magenta", "leblanc@outlook.com", "0154278473", DateTime.Now.AddYears(-5), 2000);
+            datastate.salaries.Add(chauffeur);
+
+            Client client = new Client("1", "Marseulli", "Véronique", DateTime.Now.AddYears(-40),"Rue de la Gare", "vero.m@gmail.comm", "0698528488");
+            Voiture voiture = new Voiture("AB-123-CD", "Rouge", "Peugeot", 4);
+            Commande commande = new Commande(1, client, "Paris", "Lyon", voiture, chauffeur, DateTime.Today);
+            chauffeur.LivraisonsEffectuees.Add(commande);
+
+            module.AfficherLivraisonsParChauffeur();
+
+            Assert.AreEqual(1, chauffeur.LivraisonsEffectuees.Count);
+        }
 }
